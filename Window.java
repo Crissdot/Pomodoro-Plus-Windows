7795fff95ab8b172439f718edd918b1f8b2cb6ed
crissdot <cris-2-11-2001@hotmail.com>
2021-07-08 17:41:45 -0500

    add panel on window

--- Window.java
+++ Window.java
@@ -1,17 +1,29 @@
 package pomodoroplus;
 
+import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Window extends JFrame {
     
     public Window() {
         setWindow();
+        startComponents();
     }
     
     private void setWindow() {
         setSize(500, 500);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
         setTitle("Pomodoro Plus");
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    
+    private void startComponents() {
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.GREEN);
+        
+        this.getContentPane().add(panel);
     }
     
 }
