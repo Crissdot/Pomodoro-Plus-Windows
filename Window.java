f56f7f6885b545bb1f1625eaeb42a48cdbffc54d
crissdot <cris-2-11-2001@hotmail.com>
2021-07-21 14:08:44 -0500

    add toggle function in buttons

--- Window.java
+++ Window.java
@@ -49,41 +49,48 @@ public class Window extends JFrame {
     }
     
     private void createButtons() {
-        // START
         JButton btnStart = new JButton("<HTML><U>S</U>TART</HTML>");
         btnStart.setBounds(75, 215, 350, 50);
         btnStart.setFont(new Font("arial", Font.PLAIN, 32));
         btnStart.setMnemonic('s');
         panel.add(btnStart);
         
-        ActionListener clickStart = (ActionEvent e) -> {
-            System.out.println("Test Start");
-        };
-        btnStart.addActionListener(clickStart);
-        
-        // PAUSE
         JButton btnPause = new JButton("<HTML><U>P</U>AUSE</HTML>");
         btnPause.setBounds(75, 290, 350, 50);
         btnPause.setFont(new Font("arial", Font.PLAIN, 32));
-        //btnPause.setEnabled(false);
+        btnPause.setEnabled(false);
         btnPause.setMnemonic('p');
         panel.add(btnPause);
         
-        ActionListener clickPause = (ActionEvent e) -> {
-            System.out.println("Test Pause");
-        };
-        btnPause.addActionListener(clickPause);
-        
-        // FINISH
         JButton btnFinish = new JButton("<HTML><U>F</U>INISH</HTML>");
         btnFinish.setBounds(75, 365, 350, 50);
         btnFinish.setFont(new Font("arial", Font.PLAIN, 32));
-        //btnFinish.setEnabled(false);
+        btnFinish.setEnabled(false);
         btnFinish.setMnemonic('f');
         panel.add(btnFinish);
+
+        
+        // ACTION LISTENERS
+        ActionListener clickStart = (ActionEvent e) -> {
+            btnStart.setEnabled(false);
+            btnPause.setEnabled(true);
+            btnFinish.setEnabled(true);
+        };
+        btnStart.addActionListener(clickStart);
+        
+        
+        ActionListener clickPause = (ActionEvent e) -> {
+            btnStart.setEnabled(true);
+            btnPause.setEnabled(false);
+            btnFinish.setEnabled(true);
+        };
+        btnPause.addActionListener(clickPause);
+        
         
         ActionListener clickFinish = (ActionEvent e) -> {
-            System.out.println("Test Finish");
+            btnStart.setEnabled(true);
+            btnPause.setEnabled(false);
+            btnFinish.setEnabled(false);
         };
         btnFinish.addActionListener(clickFinish);
     }
