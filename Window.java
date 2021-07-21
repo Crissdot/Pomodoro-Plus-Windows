92261f86e8d937e606a596ae942068fc95c5265e
crissdot <cris-2-11-2001@hotmail.com>
2021-07-21 14:01:44 -0500

    add actionListeners and setResizable false

--- Window.java
+++ Window.java
@@ -1,8 +1,9 @@
 package pomodoroplus;
 
 import java.awt.Color;
-import java.awt.Cursor;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,6 +20,7 @@ public class Window extends JFrame {
     
     private void setWindow() {
         setSize(500, 500);
+        setResizable(false);
         setLocationRelativeTo(null);
         setTitle("Pomodoro Plus");
         
@@ -47,25 +49,43 @@ public class Window extends JFrame {
     }
     
     private void createButtons() {
+        // START
         JButton btnStart = new JButton("<HTML><U>S</U>TART</HTML>");
         btnStart.setBounds(75, 215, 350, 50);
         btnStart.setFont(new Font("arial", Font.PLAIN, 32));
         btnStart.setMnemonic('s');
         panel.add(btnStart);
         
+        ActionListener clickStart = (ActionEvent e) -> {
+            System.out.println("Test Start");
+        };
+        btnStart.addActionListener(clickStart);
+        
+        // PAUSE
         JButton btnPause = new JButton("<HTML><U>P</U>AUSE</HTML>");
         btnPause.setBounds(75, 290, 350, 50);
         btnPause.setFont(new Font("arial", Font.PLAIN, 32));
-        btnPause.setEnabled(false);
+        //btnPause.setEnabled(false);
         btnPause.setMnemonic('p');
         panel.add(btnPause);
         
+        ActionListener clickPause = (ActionEvent e) -> {
+            System.out.println("Test Pause");
+        };
+        btnPause.addActionListener(clickPause);
+        
+        // FINISH
         JButton btnFinish = new JButton("<HTML><U>F</U>INISH</HTML>");
         btnFinish.setBounds(75, 365, 350, 50);
         btnFinish.setFont(new Font("arial", Font.PLAIN, 32));
-        btnFinish.setEnabled(false);
+        //btnFinish.setEnabled(false);
         btnFinish.setMnemonic('f');
         panel.add(btnFinish);
+        
+        ActionListener clickFinish = (ActionEvent e) -> {
+            System.out.println("Test Finish");
+        };
+        btnFinish.addActionListener(clickFinish);
     }
     
 }
