7c46a38defcd0e377b4bf15169bc641c3de229dc
crissdot <cris-2-11-2001@hotmail.com>
2021-07-26 14:32:41 -0500

    refactor buttons

---
+++ Buttons.java
@@ -0,0 +1,61 @@
+package pomodoroplus;
+
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class Buttons {
+    private final byte left = 75;
+    private final short width = 350;
+    private final byte height = 50;
+    
+    private final JButton btnStart;
+    private final JButton btnPause;
+    private final JButton btnFinish ;
+    
+    public Buttons() {
+        btnStart = createButtons("<HTML><U>S</U>TART</HTML>", (short) 215, true, 's');
+        btnPause = createButtons("<HTML><U>P</U>AUSE</HTML>", (short) 290, false, 'p');
+        btnFinish = createButtons("<HTML><U>F</U>INISH</HTML>", (short) 365, false, 'f');
+    }
+    
+    private JButton createButtons(String text, short top, boolean active, char mnenomic) {
+        JButton button = new JButton(text);
+        button.setBounds(left, top, width, height);
+        button.setEnabled(active);
+        button.setFont(new Font("arial", Font.PLAIN, 32));
+        button.setMnemonic(mnenomic);
+        
+        return button;
+    }
+
+    public JButton getBtnStart() {
+        return btnStart;
+    }
+
+    public JButton getBtnPause() {
+        return btnPause;
+    }
+
+    public JButton getBtnFinish() {
+        return btnFinish;
+    }
+    
+    public void startToggle() {
+        btnStart.setEnabled(false);
+        btnPause.setEnabled(true);
+        btnFinish.setEnabled(true);
+    }
+    
+    public void pauseToggle() {
+        btnStart.setEnabled(true);
+        btnPause.setEnabled(false);
+        btnFinish.setEnabled(true);
+    }
+    
+    public void finishToggle() {
+        btnStart.setEnabled(true);
+        btnPause.setEnabled(false);
+        btnFinish.setEnabled(false);
+    }
+    
+}
--- Window.java
+++ Window.java
@@ -13,6 +13,7 @@ import javax.swing.SwingConstants;
 public class Window extends JFrame {
     private JPanel panel;
     private JLabel counterLabel;
+    private Buttons buttons;
     
     public Window() {
         setWindow();
@@ -50,24 +51,14 @@ public class Window extends JFrame {
     }
     
     private void createButtons() {
-        JButton btnStart = new JButton("<HTML><U>S</U>TART</HTML>");
-        btnStart.setBounds(75, 215, 350, 50);
-        btnStart.setFont(new Font("arial", Font.PLAIN, 32));
-        btnStart.setMnemonic('s');
+        buttons = new Buttons();
+        
+        JButton btnStart = buttons.getBtnStart();
+        JButton btnPause = buttons.getBtnPause();
+        JButton btnFinish = buttons.getBtnFinish();
+        
         panel.add(btnStart);
-        
-        JButton btnPause = new JButton("<HTML><U>P</U>AUSE</HTML>");
-        btnPause.setBounds(75, 290, 350, 50);
-        btnPause.setFont(new Font("arial", Font.PLAIN, 32));
-        btnPause.setEnabled(false);
-        btnPause.setMnemonic('p');
         panel.add(btnPause);
-        
-        JButton btnFinish = new JButton("<HTML><U>F</U>INISH</HTML>");
-        btnFinish.setBounds(75, 365, 350, 50);
-        btnFinish.setFont(new Font("arial", Font.PLAIN, 32));
-        btnFinish.setEnabled(false);
-        btnFinish.setMnemonic('f');
         panel.add(btnFinish);
         
         addButtonFunctionality(btnStart, btnPause, btnFinish);
@@ -78,30 +69,21 @@ public class Window extends JFrame {
         
         // Start
         ActionListener clickStart = (ActionEvent e) -> {
-            btnStart.setEnabled(false);
-            btnPause.setEnabled(true);
-            btnFinish.setEnabled(true);
-            
+            buttons.startToggle();
             counter.start();
         };
         btnStart.addActionListener(clickStart);
         
         // Pause
         ActionListener clickPause = (ActionEvent e) -> {
-            btnStart.setEnabled(true);
-            btnPause.setEnabled(false);
-            btnFinish.setEnabled(true);
-            
+            buttons.pauseToggle();
             counter.pause();
         };
         btnPause.addActionListener(clickPause);
         
         // Finish
         ActionListener clickFinish = (ActionEvent e) -> {
-            btnStart.setEnabled(true);
-            btnPause.setEnabled(false);
-            btnFinish.setEnabled(false);
-            
+            buttons.finishToggle();
             counter.finish();
         };
         btnFinish.addActionListener(clickFinish);
